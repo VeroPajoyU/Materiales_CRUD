b61c8385da100cd045b0670600be6cddadb91b83
Yisel Verónica Pajoy Maca <168245554+VeroPajoyU@users.noreply.github.com>
2024-05-29 16:53:16 -0500

    Añado la ruta del crud_porcelanas

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                 <a class="nav-link" aria-current="page" href="index.php">Inicio</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Porcelanas</a>
+                                <a class="nav-link" href="crud_porcelanas.php">Porcelanas</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="crud_revestimientos.php">Revestimientos</a>
